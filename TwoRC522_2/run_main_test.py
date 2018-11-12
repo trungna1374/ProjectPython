d473c3a13d94c5bc5a162f33ab1d53d52f22e441
Nguyen Sy Trung <trungna1374@gmail.com>
2018-11-13 02:14:47 +0700

    Add RFID_2

---
+++ TwoRC522_2/run_main_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Class in Python 2.7 for testing two RFID readers with Raspberry Pi.
+
+Use: 
+$ cd TwoRC522RPi/
+$ sudo python run_main_test.py 
+Press Ctrl + z to finish.
+
+"""
+
+import sys
+from module.card_reader import CardReader
+
+def run():
+    
+    card_reader = CardReader()
+    try:
+        card_reader.start()
+    except KeyboardInterrupt:
+        print ("trl+C received! Sending kill to " , reader_card.getName())
+        if reader_card.isAlive():
+            reader_card._stopevent.set()
+
+  
+    
