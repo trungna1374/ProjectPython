d762c990ea0dd5f221bdffc1e8b1dcf27eed4de1
Nguyen Sy Trung <trungna1374@gmail.com>
2018-11-12 23:32:41 +0700

    Add servo feature

--- ServoProcess.py
+++ ServoProcess.py
@@ -1,10 +1,21 @@
+import RPi.GPIO as GPIO
+def init():
+    global pwm
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11,GPIO.OUT)
+    pwm = GPIO.PWM(11,50)
+    pwm.start(15)
+    return
 def pullUp():
+    pwm.ChangeDutyCycle(7.5)
     return
 
 
 def liftOff():
+    pwm.ChangeDutyCycle(15)
     return
 
 
 def servoProcess():
+    init()
     return
